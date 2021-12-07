05ff8bf19211216b62e2d75248f6bdc5f96854f4
Raviraj Dulange <redhood.dumb@gmail.com>
2021-12-08 04:20:16 +0530

    Add form for the transaction feature

---
+++ src/pages/Transfer/Transfer.jsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
+
+function Transfer() {
+  const navigate = useNavigate();
+  const [userDetails, setUserDetails] = useState({});
+  const [allUsers, setAllUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [amount, setAmount] = useState(0);
+  const { id } = useParams();
+  console.log(id);
+  useEffect(() => {
+    const getCurrentUserDetails = async () => {
+      let data = await axios.get(`/user/${id}`);
+      data = await data.data;
+      setUserDetails(data.user);
+    };
+    getCurrentUserDetails();
+    const getAllUserDetails = async () => {
+      let data = await axios.get("/listAllUsers");
+      data = await data.data;
+      setAllUsers(data.map((details) => details.name));
+    };
+    getAllUserDetails();
+  }, [id]);
+  const userSelectHandler = (e) => {
+    console.log(e.target.innerHTML);
+    setName(e.target.innerHTML);
+  };
+  const changeAmountHandler = (e) => {
+    setAmount(e.target.value);
+  };
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    const transferData = {
+      receiver: name,
+      sender: userDetails.name,
+      amtTransfered: parseInt(amount),
+    };
+    axios.post(`/transfer/${id}`, transferData);
+    navigate("/customersList");
+  };
+  return (
+    <div className="container d-flex col-12 mt-3">
+      <form onSubmit={onSubmitHandler}>
+        <p>Id: {userDetails.accId}</p>
+        <p>Email: {userDetails.email}</p>
+        <p>Name: {userDetails.name}</p>
+        <p>Balance: {userDetails.currentBalance}</p>
+        <div className="">
+          <Autocomplete
+            disablePortal
+            options={allUsers}
+            renderInput={(params) => <TextField {...params} label="Customer" />}
+            onChange={userSelectHandler}
+          />
+          <TextField
+            type="number"
+            id="standard-basic"
+            label="Amount"
+            variant="standard"
+            onChange={changeAmountHandler}
+          />
+        </div>
+        <Button variant="contained" type="submit" className="mt-3">
+          Make Transaction
+        </Button>
+      </form>
+    </div>
+  );
+}
+
+export default Transfer;
