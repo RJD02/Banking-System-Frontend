1011bd8ddab62df037be392d584216a824f6b197
Raviraj Dulange <redhood.dumb@gmail.com>
2021-12-08 04:18:54 +0530

    Modify App and index components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Navbar from "./components/Navbar/Navbar";
+import "./App.css";
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/Home/HomePage";
+import Customers from "./pages/Customers/Customers";
+import Transfer from "./pages/Transfer/Transfer";
+import TransactionHistory from "./pages/TransactionHistory/TransactionHistory";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/customersList" element={<Customers />} />
+        <Route path="/transfer/:id" element={<Transfer />} />
+        <Route path="/transferHistory" element={<TransactionHistory />} />
+      </Routes>
+    </>
   );
 }
 
